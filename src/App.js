a0196514cfe08662860e95ff291ea9b31a9746f6
Tucker2015 <kevtucker@gmail.com>
2020-06-07 22:10:45 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kevin Tucker
         </a>
       </header>
     </div>
