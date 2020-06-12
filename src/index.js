864ad058840f8d020f6a15d08f8048a9f301bca1
Tucker2015 <kevtucker@gmail.com>
2020-06-12 08:07:54 +0100

    Change Background Colour

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'videojs-errors';
 
 
 
- 
+
 
 
 const videoJsOptions = {
@@ -19,7 +19,7 @@ const videoJsOptions = {
   poster: './poster.jpg',
   sources: [
     {
-      src: 'https://live.ktinternet.net/mbr/hls/mbr.m3u8',
+      src: 'https://live.ktinternet.net/tbvr/hls/tbvr.m3u8',
       type: 'application/x-mpegURL',
     },
   ],
@@ -29,6 +29,7 @@ const App = () =>
 <div className="frame">
   <p>Test</p>
   <div >
+  <div class="vjs-error-display vjs-modal-dialog" aria-describedby="vjs_video_3_component_364_description" aria-hidden="false" aria-label="Modal Window" role="dialog"><p class="vjs-modal-dialog-description vjs-control-text" id="vjs_video_3_component_364_description">This is a modal window.</p><div class="vjs-modal-dialog-content" role="document">The media could not iled or because the format is not supported.</div></div>
     <Videojs {...videoJsOptions} />
   </div>
   <script src="videojs.errors.js"></script>
